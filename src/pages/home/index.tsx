d0d5d44cafef40452df82172bc4f03f3d9ca84dd
wenderf5 <wenderfabianoalves@gmail.com>
2024-06-27 14:03:19 -0300

    feat: new section top artists

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,7 @@
 import style from './index.module.css';
 import Search from '../../components/search';
 import Buttom from '../../components/buttom';
+import TopArtist from '../../components/topArtist';
 
 function Home() {
     return (
@@ -8,6 +9,17 @@ function Home() {
             <div className={style.containerSearch}>
                 <Search />
                 <Buttom type='playlist'/>
+            </div>
+            <div className={style.divTopArtists}>
+                <h2>Top Artistas</h2>
+                <div className={style.containerTopArtists}>
+                    <TopArtist />
+                    <TopArtist />
+                    <TopArtist />
+                    <TopArtist />
+                    <TopArtist />
+                    <TopArtist />
+                </div>
             </div>
         </main>
     )
